792c999e11fff616b80a9e3bfae36c7598b6f725
Karlen94 <karlen.karapetyan93@gmail.com>
2021-01-17 19:13:35 +0400

    test-3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // import PageInformation from './Page_information';
 // import SensMessage from './Get_message';
 // import Product from './homework_7/Product';
-import ToDo from './components/ToDo/ToDoList';
+import ToDo from './components/list/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
--- src/components/list/ToDoList.js
+++ src/components/list/ToDoList.js
@@ -8,7 +8,6 @@ import NewTask from '../NewTask/NewTask';
 
 class ToDo extends Component {
 
-
     state = {
         tasks: [],
         selectedTasks: new Set()
