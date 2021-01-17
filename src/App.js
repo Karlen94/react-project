043650363d0f3749a9272b753fb6fe3a41079c49
Karlen94 <karlen.karapetyan93@gmail.com>
2021-01-17 18:57:50 +0400

    problem with folder ToDo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 // import PageInformation from './Page_information';
 // import SensMessage from './Get_message';
 // import Product from './homework_7/Product';
-import ToDo from './components/toDo/ToDoList';
+import ToDo from './components/ToDo/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
