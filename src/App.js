b954cf4e712978e1b86268b1d7ee25494570c33b
Karlen94 <karlen.karapetyan93@gmail.com>
2021-01-18 17:27:57 +0400

    Modal components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 // import PageInformation from './Page_information';
 // import SensMessage from './Get_message';
 // import Product from './homework_7/Product';
-import ToDo from './components/papka/ToDoList';
+import ToDo from './components/ToDo/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
