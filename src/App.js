686b01cfabb901fa3aba227d1ba2675378424142
Karlen94 <karlen.karapetyan93@gmail.com>
2021-02-06 23:54:35 +0400

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import './App.css';
 // import Product from './homework_7/Product';
 import ToDo from './components/ToDo/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/pages/About/About';
+import Contact from './components/pages/Contact/Contact';
+import NotFound from './components/pages/NotFound/NotFound';
+import NavMenu from './components/NavMenu/NavMenu';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 
 
@@ -15,7 +20,39 @@ function App() {
   return (
     <div className="App">
 
-      <ToDo />
+      <NavMenu />
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path='/'
+            component={ToDo}
+            exact={true}
+          />
+          <Route
+            path='/home'
+            component={ToDo}
+            exact={true}
+          />
+          <Route
+            path='/about'
+            component={About}
+            exact={true}
+          />
+          <Route
+            path='/contact'
+            component={Contact}
+            exact={true}
+          />
+          <Route
+            path='/not-found'
+            component={NotFound}
+            exact={true}
+          />
+
+          <Redirect to='/not-found' />
+        </Switch>
+      </BrowserRouter>
+
 
     </div>
   );
---
+++ src/components/NavMenu/NavMenu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+
+export default function NavMenu() {
+
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Navbar>
+
+    );
+}
