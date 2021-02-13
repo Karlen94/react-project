a8d749ca2543890532cfb1a06be3b92643ce2a45
Karlen94 <karlen.karapetyan93@gmail.com>
2021-02-13 19:21:26 +0400

    focus function

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function Contact() {
 
     return (
-        <h1>Contact us page</h1>
-
+        <div>
+            <h1>Contact us page</h1>
+            <input
+                type='Email'
+            />
+            <input
+                type='Email'
+            />
+            <textarea />
+        </div>
     );
 }
