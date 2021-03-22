252789e047c5e13419a3a3f43c3f113a6c945c97
Karlen94 <karlen.karapetyan93@gmail.com>
2021-03-22 20:34:32 +0400

    fix bag

--- src/helpersFunctions/storage.js
+++ src/helpersFunctions/storage.js
@@ -1,5 +1,5 @@
 export const getToken = () => {
-    const token = localStorage.getItem('token');
+    // const token = localStorage.getItem('token');
 }
 
 export const checkLoginStatus = () => !!localStorage.getItem('token');
